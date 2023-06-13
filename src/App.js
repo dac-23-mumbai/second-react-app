c703d83907367bbf07be344e9600ab80f59ba4ed
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-13 09:15:07 +0530

    hello.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return <h1>Hello</h1>;
 }
 
+// UNUSED!!!
 // JSX => JS + HTML/XML
 // IF your function returns HTML
 // Convention:: Start your function name in captial letter.
